c1898486d4219bf05593ef012561e2612eaac3fe
Sarita <saritapatel0729@gmail.com>
2025-08-05 00:09:23 +0200

    updated ConfugurationReader class

--- src/test/java/utilities/ConfigurationReader.java
+++ src/test/java/utilities/ConfigurationReader.java
@@ -1,40 +1,56 @@
 package utilities;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.Properties;
 
 public class ConfigurationReader {
-    private static Properties properties;
+
+    private static Properties properties = new Properties();
 
     static {
-
-        try {
-            String path = "configuration.properties";
-            FileInputStream input = new FileInputStream(path);
-            properties = new Properties();
+        try (InputStream input = ConfigurationReader.class.getClassLoader()
+                .getResourceAsStream("configuration.properties")) {
+            if (input == null) {
+                throw new FileNotFoundException("configuration.properties file not found in resources.");
+            }
             properties.load(input);
-            input.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public static String get(String keyName){
-        return properties.getProperty(keyName);
-    }
-
-    public static void set(String keyName, String value){
-        String path = "configuration.properties";
-
-        try {
-            OutputStream output = new FileOutputStream(path);
-            properties.setProperty(keyName, value);
-            properties.store(output,null);
-        }catch (Exception e){
-            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load configuration.properties", e);
         }
     }
+
+    public static String get(String key) {
+        return properties.getProperty(key);
+    }
+//    private static Properties properties;
+//
+//    static {
+//
+//        try {
+//            String path = "configuration.properties";
+//            FileInputStream input = new FileInputStream(path);
+//            properties = new Properties();
+//            properties.load(input);
+//            input.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+//
+//    public static String get(String keyName){
+//        return properties.getProperty(keyName);
+//    }
+//
+//    public static void set(String keyName, String value){
+//        String path = "configuration.properties";
+//
+//        try {
+//            OutputStream output = new FileOutputStream(path);
+//            properties.setProperty(keyName, value);
+//            properties.store(output,null);
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
 }

6f0f2d8dd963ed43b00e380334040a591baf6f06
Sarita <saritapatel0729@gmail.com>
2025-08-06 11:44:15 +0200

    updated ConfigurationReader and yaml file

--- src/test/java/utilities/ConfigurationReader.java
+++ src/test/java/utilities/ConfigurationReader.java
@@ -5,36 +5,79 @@ import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.util.Properties;
 
+import java.io.*;
+import java.util.Properties;
+
 public class ConfigurationReader {
     private static Properties properties;
 
     static {
-
         try {
-            String path = "configuration.properties";
-            FileInputStream input = new FileInputStream(path);
             properties = new Properties();
+            InputStream input = ConfigurationReader.class.getClassLoader()
+                    .getResourceAsStream("configuration.properties");
+
+            if (input == null) {
+                throw new FileNotFoundException("configuration.properties not found in classpath");
+            }
+
             properties.load(input);
             input.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
-    public static String get(String keyName){
+    public static String get(String keyName) {
         return properties.getProperty(keyName);
     }
 
-    public static void set(String keyName, String value){
-        String path = "configuration.properties";
-
+    // Optional: You can keep this, but it won't work with classpath-based file loading.
+    public static void set(String keyName, String value) {
         try {
-            OutputStream output = new FileOutputStream(path);
+            // This only works if the file is in the file system, not inside a JAR or classpath
+            File file = new File("src/test/resources/configuration.properties");
+            FileOutputStream output = new FileOutputStream(file);
+
             properties.setProperty(keyName, value);
-            properties.store(output,null);
-        }catch (Exception e){
+            properties.store(output, null);
+            output.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
+
+//public class ConfigurationReader {
+//    private static Properties properties;
+//
+//    static {
+//
+//        try {
+//            String path = "configuration.properties";
+//            FileInputStream input = new FileInputStream(path);
+//            properties = new Properties();
+//            properties.load(input);
+//            input.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+//
+//    public static String get(String keyName){
+//        return properties.getProperty(keyName);
+//    }
+//
+//    public static void set(String keyName, String value){
+//        String path = "configuration.properties";
+//
+//        try {
+//            OutputStream output = new FileOutputStream(path);
+//            properties.setProperty(keyName, value);
+//            properties.store(output,null);
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
+//}
